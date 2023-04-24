b5a73bbdff395a85a7e1ac2c21ce62dd999ec94a
Hira-Yuki <cjsdlf44@gmail.com>
2023-04-24 20:50:08 +0900

    useEffect import.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import React, { useEffect } from 'react';
 import "./App.css";
 import DoneList from "./Components/DoneList";
 import WorkingList from "./Components/WorkingList";
