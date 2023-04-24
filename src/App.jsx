f5394a3ca3219d7ff2e14ceed60064f6d97d1595
Hira-Yuki <cjsdlf44@gmail.com>
2023-04-24 20:51:47 +0900

    오류 구문 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 import DoneList from "./Components/DoneList";
 import WorkingList from "./Components/WorkingList";
